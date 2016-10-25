12984a0224ab583c8d9da5de26c8e76a3099d510
James Jurach <james.jurach@gmail.com>
2016-10-25 01:40:57 -0500

    re-organize cmd. add test

---
+++ daemon_test.go
@@ -0,0 +1,11 @@
+package gagent
+
+import (
+	"fmt"
+	//"github.com/jjurach/gagent/daemon"
+	"testing"
+)
+
+func TestYadda(t *testing.T) {
+	fmt.Println("TODO: choose sequence of random ports and create in-process server/client?")
+}
